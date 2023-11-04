21dfcff898613f17dff2ff2668953b4fe52c2d3c
marcusholloway17 <loganjean17@gmail.com>
2023-11-04 22:06:35 +0000

    declare endpoints

---
+++ src/constants/endpoint.ts
@@ -0,0 +1,9 @@
+export const endpoints = {
+  third_parties: "/api/client/third-parties",
+  req_logs: "/req-logs",
+  request_count: "/request-count",
+  error_logs: "/error-logs",
+  error_count: "/error-count",
+  authorized_clients: "/authorized-clients",
+  authorized_clients_revoke: "/revoke",
+};
