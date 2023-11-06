f0924f7f9ebd73dfec7a23a2f979ae4d6f25b104
marcusholloway17 <loganjean17@gmail.com>
2023-11-06 18:16:32 +0000

    rewrite test

--- src/tests/connect.test.ts
+++ src/tests/connect.test.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
-import { ThirdPartyClient } from "..";
 dotenv.config();
+import { ThirdPartyClient } from "..";
 
 const app = new ThirdPartyClient({
   clientHost: process.env.CLIENT_HOST ?? "",
@@ -8,12 +8,16 @@ const app = new ThirdPartyClient({
   clientSecret: process.env.CLIENT_SECRET ?? "",
 });
 
-console.log("app", app)
-
 test("CLIENT_CONNECTION_TEST", async () => {
-  expect(await app?.connect()).toStrictEqual({
+  expect(
+    await app?.connect({
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  ).toStrictEqual({
     key: "1ed83457-30fb-11ee-a755-0242ac110005",
-    secret: "$2y$10$aSfxPP.2OAkEwi1MG5N1eOvaQBsFLOgFdasdvkqdlJUnTVuMucYZC",
+    secret: null,
     name: "AUTH",
     description: "AUTHENTICATION WEB SERVICE",
     host: "https://auth.sedana.io",
@@ -25,13 +29,13 @@ test("CLIENT_CONNECTION_TEST", async () => {
     allowRegistration: true,
     onlyVerified: true,
     useCustom: true,
-    lastActivity: "2023-11-06T02:07:41.068Z",
+    lastActivity: "2023-11-06T14:18:47.855Z",
     active: true,
     adminId: "df229f73-30f9-11ee-a755-0242ac110005",
     activatedBy: null,
     activatedAt: null,
-    createdAt: "2023-11-02T16:06:30.000Z",
-    updatedAt: "2023-11-06T02:07:41.070Z",
+    createdAt: "2023-09-28T17:29:43.000Z",
+    updatedAt: "2023-11-06T14:18:47.855Z",
     deletedAt: null,
   });
 });
